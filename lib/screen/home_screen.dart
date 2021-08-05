5030786db020b984a3f3f104f519dcdaf6e45fa3
Jules Chevalier <jchevalier@peaks.fr>
2021-08-05 11:43:26 +0200

    chore: cleanup default project

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: Text(
+        'It\'s Git Time!',
+        style: TextStyle(fontSize: 64, fontWeight: FontWeight.bold),
+      )),
+    );
+  }
+}
