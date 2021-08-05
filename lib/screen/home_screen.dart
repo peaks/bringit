37389c7f5137ea659d9c24bdc179ad472e83a325
Jules Chevalier <jchevalier@peaks.fr>
2021-08-05 18:10:33 +0200

    feat: simulate git commandes from GUI

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +12,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _cmdController = TextEditingController();
+  final FocusNode _cmdFocus = FocusNode();
+  final TextEditingController _stdoutController = TextEditingController();
+  final TextEditingController _stderrController = TextEditingController();
+  bool _lastCommandSuccedeed = true;
+
+  void _runCommand() {
+    final ProcessResult result = Process.runSync(
+        'git', _cmdController.text.split(' '),
+        workingDirectory: './test/test_repository');
+    _stdoutController.text = result.stdout.toString();
+    _stderrController.text = result.stderr.toString();
+    setState(() {
+      _lastCommandSuccedeed = result.exitCode == 0;
+    });
+    _cmdController.clear();
+    _cmdFocus.requestFocus();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,13 +39,75 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Center(
-              child: Column(children: const <Widget>[
-            Text(
-              'It\'s Git Time!',
-              style: TextStyle(fontSize: 64, fontWeight: FontWeight.bold),
-            ),
-          ]))),
+          child: Column(children: [
+            Column(children: <Widget>[
+              const Text(
+                'It\'s Git Time!',
+                style: TextStyle(fontSize: 64, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                children: <Widget>[
+                  const Text(
+                    'git',
+                    style: TextStyle(fontSize: 32),
+                    textAlign: TextAlign.right,
+                  ),
+                  Expanded(
+                      child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: TextField(
+                            controller: _cmdController,
+                            focusNode: _cmdFocus,
+                            style: const TextStyle(fontSize: 32),
+                            textAlign: TextAlign.left,
+                            decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: _lastCommandSuccedeed
+                                        ? Colors.greenAccent
+                                        : Colors.redAccent,
+                                    width: 0.0),
+                              ),
+                            ),
+                            onSubmitted: (_) => _runCommand(),
+                          ))),
+                  TextButton(
+                      onPressed: () => _runCommand(),
+                      child: const Icon(Icons.keyboard_return))
+                ],
+              ),
+              TextField(
+                controller: _stdoutController,
+                enabled: false,
+                style: const TextStyle(fontSize: 24),
+                textAlign: TextAlign.left,
+                minLines: 20,
+                maxLines: 20,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: _lastCommandSuccedeed
+                              ? Colors.greenAccent
+                              : Colors.redAccent)),
+                ),
+              ),
+              TextField(
+                controller: _stderrController,
+                enabled: false,
+                style: const TextStyle(fontSize: 24, color: Colors.redAccent),
+                textAlign: TextAlign.left,
+                minLines: 2,
+                maxLines: 2,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: _lastCommandSuccedeed
+                              ? Colors.greenAccent
+                              : Colors.redAccent)),
+                ),
+              )
+            ])
+          ])),
     );
   }
 }
