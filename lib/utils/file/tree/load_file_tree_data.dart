e1659fd5aab8a582e80c8892b1fe8d4e85d30ed5
Jules Chevalier <jchevalier@peaks.fr>
2022-03-18 17:12:22 +0100

    refactor: minimize code and fix tree depth access

--- lib/utils/file/tree/load_file_tree_data.dart
+++ lib/utils/file/tree/load_file_tree_data.dart
@@ -1,30 +1,11 @@
 import 'dart:io';
 
-/// recursive function to add node
-List<Map<String, dynamic>> addToParentNode(List<Map<String, dynamic>> nodes,
-    Map<String, dynamic> n, Iterable<String> pathToFile) {
-  if (pathToFile.isNotEmpty) {
-    for (final Map<String, dynamic> node in nodes) {
-      if (node['label'] == pathToFile.first) {
-        node['children'] = addToParentNode(
-            node['children'] as List<Map<String, dynamic>>,
-            n,
-            pathToFile.where((String element) => element != pathToFile.first));
-      }
-    }
-  } else {
-    nodes.add(n);
-  }
-
-  /// sort nodes before render
-  nodes.sort((Map<String, dynamic> a, Map<String, dynamic> b) =>
-      (a['key'] as String).compareTo(b['key'] as String));
-  return nodes;
-}
+import 'package:flutter_treeview/flutter_treeview.dart';
+import 'package:path/path.dart';
 
 /// get name of a file by uri
 String getFileNameByUri(FileSystemEntity file) {
-  if (file.uri.toString().contains('/')) {
+  if (file.absolute.path.toString().contains('/')) {
     final List<String> temp = file.uri.toString().split('/');
     if (temp.isNotEmpty && temp.last != '') {
       return temp.last;
@@ -36,33 +17,28 @@ String getFileNameByUri(FileSystemEntity file) {
   }
 }
 
-List<Map<String, dynamic>> getDirFiles(String path, bool docsOpen) {
-  /// Create Dir object from existing path
-  final Directory myDir = Directory(path);
-
+List<Node<void>> getNodesFromPath(String path, bool docsOpen) {
   /// List all file from dir
-  final List<FileSystemEntity> allContent = myDir.listSync(recursive: true);
+  final Directory directory = Directory(path);
+  final List<FileSystemEntity> allContent =
+      directory.listSync(recursive: false);
 
   /// result
-  List<Map<String, dynamic>> nodes = <Map<String, dynamic>>[];
+  final List<Node<void>> nodes = <Node<void>>[];
 
   /// loop items in path
   for (final FileSystemEntity file in allContent) {
-    final Map<String, dynamic> n = <String, dynamic>{
-      'label': getFileNameByUri(file),
-      'key': file.uri.toString(),
-      'children': <Map<String, dynamic>>[]
-    };
+    final Node<void> node = Node<void>(
+      key: file.absolute.path,
+      label: basename(file.path),
+      children: file is Directory
+          ? getNodesFromPath(file.absolute.path, docsOpen)
+          : <Node<void>>[],
+    );
 
-    /// file is located on root of tree
-    if (file.parent.path == '.' || file.parent.path == './') {
-      nodes.add(n);
-    } else {
-      final Iterable<String> pathToFile = n['key'].toString().split('/').where(
-          (String element) => element.toString() != n['label'].toString());
-      nodes = addToParentNode(nodes, n, pathToFile);
-    }
+    nodes.add(node);
   }
+  //nodes.sort((Node<void> a, Node<void> b) => (a.key).compareTo(b.key));
 
   return nodes;
 }
--- lib/widgets/file_tree.dart
+++ lib/widgets/file_tree.dart
@@ -1,11 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_treeview/flutter_treeview.dart';
 import 'package:git_ihm/utils/file/tree/load_file_tree_data.dart';
 
-/// package : flutter_treeview_example
-/// doc-url : https://bitbucket.org/kevinandre/flutter_treeview_example/src/master/
 class FileTree extends StatefulWidget {
   const FileTree({Key? key, required this.path}) : super(key: key);
   final String path;
@@ -19,47 +15,16 @@ class _MyHomePageState extends State<FileTree> {
   late TreeViewController _treeViewController;
   bool docsOpen = true;
   bool deepExpanded = true;
-  final Map<ExpanderPosition, Widget> expansionPositionOptions =
-      const <ExpanderPosition, Text>{
-    ExpanderPosition.start: Text('Start'),
-    ExpanderPosition.end: Text('End'),
-  };
-  final Map<ExpanderType, Widget> expansionTypeOptions = <ExpanderType, Widget>{
-    ExpanderType.none: Container(),
-    ExpanderType.caret: const Icon(
-      Icons.arrow_drop_down,
-      size: 28,
-    ),
-    ExpanderType.arrow: const Icon(Icons.arrow_downward),
-    ExpanderType.chevron: const Icon(Icons.expand_more),
-    ExpanderType.plusMinus: const Icon(Icons.add),
-  };
-  final Map<ExpanderModifier, Widget> expansionModifierOptions =
-      <ExpanderModifier, Widget>{
-    ExpanderModifier.none: const ModContainer(modifier: ExpanderModifier.none),
-    ExpanderModifier.circleFilled:
-        const ModContainer(modifier: ExpanderModifier.circleFilled),
-    ExpanderModifier.circleOutlined:
-        const ModContainer(modifier: ExpanderModifier.circleOutlined),
-    ExpanderModifier.squareFilled:
-        const ModContainer(modifier: ExpanderModifier.squareFilled),
-    ExpanderModifier.squareOutlined:
-        const ModContainer(modifier: ExpanderModifier.squareOutlined),
-  };
   final bool _allowParentSelect = false;
   final bool _supportParentDoubleTap = false;
   final ScrollController _scrollController = ScrollController();
   @override
   void initState() {
-    /// controller
     _treeViewController = TreeViewController(
-      children: <Node<dynamic>>[],
+      children: getNodesFromPath(widget.path, docsOpen),
       selectedKey: _selectedNode,
     );
-    _treeViewController = _treeViewController.loadJSON<dynamic>(
-        json: jsonEncode(getDirFiles(widget.path, docsOpen)));
 
-    /// call parent init state
     super.initState();
   }
 
@@ -67,21 +32,18 @@ class _MyHomePageState extends State<FileTree> {
   Widget build(BuildContext context) {
     final TreeViewTheme _treeViewTheme = TreeViewTheme(
       expanderTheme: const ExpanderThemeData(
-          type: ExpanderType.arrow,
-          modifier: ExpanderModifier.none,
-          position: ExpanderPosition.start,
-          // color: Colors.grey.shade800,
-          size: 20,
-          color: Colors.blue),
+        type: ExpanderType.chevron,
+        position: ExpanderPosition.start,
+        size: 20,
+      ),
       labelStyle: const TextStyle(
         fontSize: 16,
-        letterSpacing: 0.3,
+        fontFamily: 'FantasqueSansMono',
       ),
-      parentLabelStyle: TextStyle(
+      parentLabelStyle: const TextStyle(
+        fontFamily: 'FantasqueSansMono',
+        fontWeight: FontWeight.bold,
         fontSize: 16,
-        letterSpacing: 0.1,
-        fontWeight: FontWeight.w800,
-        color: Colors.blue.shade700,
       ),
       iconTheme: IconThemeData(
         size: 18,
@@ -89,110 +51,38 @@ class _MyHomePageState extends State<FileTree> {
       ),
       colorScheme: Theme.of(context).colorScheme,
     );
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Files in ${widget.path}'),
-        elevation: 0,
-      ),
-      body: GestureDetector(
-        onTap: () {
-          FocusScope.of(context).requestFocus(FocusNode());
-        },
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          height: double.infinity,
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: Scrollbar(
-                  controller: _scrollController, // <---- Here, the controller
-                  isAlwaysShown: true, // <---- Required
-                  child: SingleChildScrollView(
-                    controller: _scrollController,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: SingleChildScrollView(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: const EdgeInsets.all(10),
-                          child: TreeView(
-                              controller: _treeViewController,
-                              allowParentSelect: _allowParentSelect,
-                              supportParentDoubleTap: _supportParentDoubleTap,
-                              onExpansionChanged: (String key, bool expanded) {
-                                setState(() {
-                                  docsOpen = expanded;
-                                });
-                              },
-                              onNodeTap: (String key) {
-                                debugPrint('Selected: $key');
-                                setState(() {
-                                  _selectedNode = key;
-                                  _treeViewController = _treeViewController
-                                      .copyWith<dynamic>(selectedKey: key);
-                                });
-                              },
-                              theme: _treeViewTheme,
-                              shrinkWrap: true),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+    return Container(
+      height: double.infinity,
+      child: Scrollbar(
+        controller: _scrollController,
+        isAlwaysShown: true,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          controller: _scrollController,
+          child: TreeView(
+              controller: _treeViewController,
+              allowParentSelect: _allowParentSelect,
+              supportParentDoubleTap: _supportParentDoubleTap,
+              onExpansionChanged: (String key, bool expanded) {
+                setState(() {
+                  docsOpen = expanded;
+                });
+              },
+              onNodeTap: (String key) {
+                setState(() {
+                  if (_selectedNode == key) {
+                    _selectedNode = '';
+                  } else {
+                    _selectedNode = key;
+                  }
+                  _treeViewController = _treeViewController.copyWith<dynamic>(
+                      selectedKey: _selectedNode);
+                });
+              },
+              theme: _treeViewTheme,
+              shrinkWrap: true),
         ),
       ),
-    );
-  }
-}
-
-class ModContainer extends StatelessWidget {
-  /// constructor
-  const ModContainer({Key? key, required this.modifier}) : super(key: key);
-
-  /// modifier
-  final ExpanderModifier modifier;
-  @override
-  Widget build(BuildContext context) {
-    double _borderWidth = 0;
-    BoxShape _shapeBorder = BoxShape.rectangle;
-    Color _backColor = Colors.transparent;
-    final Color _backAltColor = Colors.grey.shade700;
-    switch (modifier) {
-      case ExpanderModifier.none:
-        break;
-      case ExpanderModifier.circleFilled:
-        _shapeBorder = BoxShape.circle;
-        _backColor = _backAltColor;
-        break;
-      case ExpanderModifier.circleOutlined:
-        _borderWidth = 1;
-        _shapeBorder = BoxShape.circle;
-        break;
-      case ExpanderModifier.squareFilled:
-        _backColor = _backAltColor;
-        break;
-      case ExpanderModifier.squareOutlined:
-        _borderWidth = 1;
-        break;
-    }
-    return Container(
-      decoration: BoxDecoration(
-        shape: _shapeBorder,
-        border: _borderWidth == 0
-            ? null
-            : Border.all(
-                width: _borderWidth,
-                color: _backAltColor,
-              ),
-        color: _backColor,
-      ),
-      width: 15,
-      height: 15,
     );
   }
 }
