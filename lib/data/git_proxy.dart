45839156377883ed3ef00ab9d11e14f3a56ade10
lreus <lreus@peaks.fr>
2022-04-06 16:36:19 +0200

    feat(injection): inject GitStatusCommand into GitProxy - Implement method gitStatus in abstract class - Test delegation of method GitStatus to GitStatusCommand - Implement GitRegistry class responsible to instantiate GitStatusCommand - Extract GitProxyMock from path_selector_test and implement new method gitStatus

--- lib/data/git_proxy.dart
+++ lib/data/git_proxy.dart
@@ -1,20 +1,35 @@
 import 'package:args/args.dart';
 
 import 'package:git/git.dart';
+import 'package:git_ihm/data/git/git_status_command.dart';
+import 'package:git_ihm/data/git/status_file.dart';
+import 'package:git_ihm/git/git_registry.dart';
 
 abstract class GitProxy {
   Future<bool> isGitDir(String path);
+
+  Future<List<StatusFile>> gitStatus(String path);
 }
 
 class GitProxyImplementation implements GitProxy {
+  GitProxyImplementation(this.statusCommand);
+
+  final GitStatusCommand statusCommand;
+
   @override
   Future<bool> isGitDir(String path) async {
     return await GitDir.isGitDir(path);
   }
+
+  @override
+  Future<List<StatusFile>> gitStatus(String path) async {
+    return statusCommand.run(path);
+  }
 }
 
 Future<void> main(List<String> arguments) async {
-  final GitProxyImplementation git = GitProxyImplementation();
+  final GitRegistry registry = GitRegistry();
+  final GitProxyImplementation git = GitProxyImplementation(registry.gitStatusCommand);
   final String path = getPathFromArguments(arguments);
   final bool gitDir = await git.isGitDir(path);
 
---
+++ test/data/git_proxy_test.dart
@@ -0,0 +1,54 @@
+import 'package:git_ihm/data/git/git_status_command.dart';
+import 'package:git_ihm/data/git/status_file.dart';
+import 'package:git_ihm/data/git_proxy.dart';
+import 'package:git_ihm/git/git_registry.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('it delegates gitStatus to GitStatusCommand', () async {
+    final GitRegistryMock mockedRegistry = GitRegistryMock();
+    final GitStatusCommandMock gitStatusCommandMock = mockedRegistry.gitStatusCommand;
+    final GitProxy testSubject = GitProxyImplementation(gitStatusCommandMock);
+
+
+    const String gitPath = '/foo/bar';
+    const List<StatusFile> commandResult = <StatusFile>[
+      UntrackedPath('any/path')
+    ];
+
+    gitStatusCommandMock.withParameter(gitPath).willReturn(commandResult);
+
+    expect(await testSubject.gitStatus(gitPath), same(commandResult));
+  });
+}
+
+class GitStatusCommandMock extends GitStatusCommand
+{
+  List<StatusFile> mockedResult = <StatusFile>[];
+  String expectedParameter = '';
+
+  @override
+  Future<List<StatusFile>> run(String path) async {
+    if (path != expectedParameter) {
+      throw Exception('Failed asserting method "run" parameter: expecting "$expectedParameter", actual: "$path"');
+    }
+    return mockedResult;
+  }
+
+  void willReturn(List<StatusFile> list) {
+    mockedResult = list;
+  }
+
+  GitStatusCommandMock withParameter(String gitPath) {
+    expectedParameter = gitPath;
+
+    return this;
+  }
+}
+
+class GitRegistryMock extends GitRegistry {
+  final GitStatusCommandMock _gitStatusMock = GitStatusCommandMock();
+
+  @override
+  GitStatusCommandMock get gitStatusCommand => _gitStatusMock;
+}
