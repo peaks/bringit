a67536c19fcea360f5f31562a7a347d0eb18b0a8
lreus <lreus@peaks.fr>
2022-05-18 10:33:53 +0200

    refactor(StatusFetcher): remove useless dependency on Git library
    - Delegate directly to ProcessResult the call on "git status" as GitDir does not provide any improvements

--- lib/git/base_command/status_fetcher.dart
+++ lib/git/base_command/status_fetcher.dart
@@ -1,21 +1,20 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:git/git.dart';
 import 'package:git_ihm/git/base_command/base_command.dart';
 
 class StatusFetcher extends BaseCommand {
   static const LineSplitter _splitter = LineSplitter();
 
   Future<List<String>> fetch(String path) async {
-    final GitDir git = await GitDir.fromExisting(path);
-    final String results = await _callGitStatus(git);
+    final String results = await _callGitStatus(path);
     return _splitter.convert(results);
   }
 
-  Future<String> _callGitStatus(GitDir git) async {
-    final ProcessResult result = await git
-        .runCommand(<String>['status', '--porcelain=v1', '--ignored=matching']);
+  Future<String> _callGitStatus(String path) async {
+    final ProcessResult result = await Process.run(
+        'git', <String>['status', '--porcelain=v1', '--ignored=matching'],
+        workingDirectory: path);
 
     if (commandIsSuccessful(result)) {
       return result.stdout as String;
