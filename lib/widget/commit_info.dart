5987b40256a8593fb5590f498e57e92c600921fb
Thibault Rousset <trousset@peaks.fr>
2023-02-16 14:53:01 +0100

    refactor: flutter upgrade

--- lib/widget/commit_info.dart
+++ lib/widget/commit_info.dart
@@ -11,7 +11,7 @@ class CommitInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: ExpansionTile(
-        // shape: InputBorder.none,
+        shape: InputBorder.none,
         tilePadding: EdgeInsets.zero,
         controlAffinity: ListTileControlAffinity.leading,
         iconColor: NordColors.$4,
